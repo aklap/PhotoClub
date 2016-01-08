4958540a0d805ca469033ba13a6a02e01f0ab3ae
Alexis La Porte <aklaporte@gmail.com>
2016-01-08 16:25:18 -0500

    add paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107205930) do
+ActiveRecord::Schema.define(version: 20160108212128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160107205930) do
     t.string   "caption",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
