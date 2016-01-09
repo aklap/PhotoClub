a720bbaa57d530e42a68b981ba7f812516542666
Alexis La Porte <aklaporte@gmail.com>
2016-01-08 19:24:25 -0500

    add create method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController<ApplicationController
   def index
-
   end
 
   def new
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
   end
 end
