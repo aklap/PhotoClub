40e85c40b98b6c3cdccb3372a82c7d74621712cb
Alexis La Porte <aklaporte@gmail.com>
2016-01-11 00:19:13 -0500

    create user auth spec

--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -1,3 +1,6 @@
+require 'rails_helper'
+require 'spec_helper'
+
 feature 'Creating a new user' do
   background do
     visit '/'
@@ -5,11 +8,35 @@ feature 'Creating a new user' do
   end
 
   scenario 'can create a new user via the index page' do
-    fill_in 'User name' with: 'sxyrailsdev'
-    fill_in 'Email' with: 'sxyrailsdev@myspace.com'
-    fill_in 'Password' with: 'supersecret'
-    fill_in 'Password confirmation' with: 'supersecret'
-    click_on 'Sign up'
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-   end
+    fill_in 'Username', with: 'sxyrailsdev'
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :prefer_exact
+    fill_in 'Password confirmation', with: 'supersecret', match: :prefer_exact
+    click_on 'Sign Up'
+
+    expect(page.current_path).to eq '/posts'
+    expect(page).to have_content 'Welcome! Registration successful.'
+  end
+
+  scenario 'requires a user name to successfully create an account' do
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :prefer_exact
+    fill_in 'Password confirmation', with: 'supersecret', match: :prefer_exact
+    click_on 'Sign Up'
+
+    expect(page).to have_content "Username can't be blank"
+  end
+
+  scenario 'requires a username to be more than 4 characters' do
+    fill_in 'Username', with: 'h'
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :prefer_exact
+    fill_in 'Password confirmation', with: 'supersecret', match: :prefer_exact
+    click_on 'Sign Up'
+
+    expect(page).to have_content 'Username is too short (minimum is 4 characters)'
+
+
+  end
 end
